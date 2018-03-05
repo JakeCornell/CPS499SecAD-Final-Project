9ae5ff75e45a04493572981f11434ee3c17b92b0
Jake Cornell <Cornellj1@udayton.edu>
2018-03-05 11:17:07 -0800

    added assignment 2 basecode

---
+++ assignments/assignment2/EchoServer.java
@@ -0,0 +1,113 @@
+import java.net.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.*;
+
+public class EchoServer {
+    static ThreadList threadlist = new ThreadList();
+    public static void main(String[] args) throws IOException {
+        
+        if (args.length != 1) {
+            System.err.println("Usage: java EchoServer <port number>");
+            System.exit(1);
+        }
+        
+        int portNumber = Integer.parseInt(args[0]);
+        
+        try {
+            ServerSocket serverSocket =
+                new ServerSocket(Integer.parseInt(args[0]));
+	    while(true){
+            	System.out.println("EchoServer is running at port " + Integer.parseInt(args[0]));
+            	Socket clientSocket = serverSocket.accept(); 
+           	System.out.println("A client is connected ");
+		EchoServerThread newthread = new EchoServerThread(threadlist, clientSocket);
+		threadlist.addThread(newthread);
+		newthread.start();
+	    }
+         } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+class EchoServerThread extends Thread{
+	private Socket clientSocket = null;
+	private PrintWriter out = null;
+	ThreadList threadlist = null;
+	public EchoServerThread(Socket socket){
+		clientSocket = socket;
+	}
+	public EchoServerThread(ThreadList threadlist, Socket socket){
+		this.threadlist = threadlist;
+		clientSocket = socket;
+	}
+
+	public void run(){
+		System.out.println("A new thread for client is running");
+		System.out.println("Inside Thread: Total Clients: " + threadlist.getNumberofThreads());
+	try {
+	    	
+	    PrintWriter out =
+                new PrintWriter(clientSocket.getOutputStream(), true);
+	    	
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("received from client: " + inputLine);
+                System.out.println("Echo back");
+                //out.println(inputLine);
+		if(inputLine.equals("<exit>")){
+			threadlist.sendToAll("To All: A client exists, the number of connected client:" + (threadlist.getNumberofThreads()-1));
+			threadlist.removeThread(this);
+			clientSocket.close();
+		}else{
+			threadlist.sendToAll("To All <new message>:"+ inputLine);
+		}
+	    }
+        } catch (IOException e) {
+            
+            System.out.println(e.getMessage());
+        }
+	
+	}
+	public void send(String message){
+	    if(out != null){
+		out.println(message);
+	    }
+        }
+
+    } 
+
+
+class ThreadList{
+	private ArrayList<EchoServerThread> threadlist = new ArrayList<EchoServerThread>(); //store the list of threads in this variable	
+	public ThreadList(){		
+	}
+	public synchronized int getNumberofThreads(){
+		return threadlist.size();	
+	}
+	public synchronized void addThread(EchoServerThread newthread){
+		threadlist.add(newthread);	//add the newthread object to the threadlist	
+	}
+	public synchronized void removeThread(EchoServerThread thread){
+		threadlist.remove(thread);	//remove the given thread from the threadlist		
+	}
+	public synchronized void sendToAll(String message){
+		/*for(int i = 0; i < getNumberofThreads(); i++){
+			if(message != null){
+				thread.send(message);
+			}
+		}*/
+		for (EchoServerThread thread : threadlist){
+			thread.send(message);
+		}
+		//ask each thread in the threadlist to send the given message to its client		
+	}
+}
+
+
---
+++ labs/lab5/basic.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+
+<?php
+
+echo "Welcome to my PHP test page!<br>"; // generate test to HTML page
+
+$x = 'SecAD Spring'; # variables must start with $ and case-sensitive
+
+$y = 2018; //no type declaration for variables 
+
+/* PHP keywords are NOT case-sensitive */
+
+PrInt "From $x " . $y . '<br>'; 
+
+?>
+
+</body>
+
+</html>
+
+
